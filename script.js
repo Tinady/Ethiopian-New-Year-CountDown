12abfb0fb1fd16aaaa38b277170d3caaa593db3d
Tina Dessu <tinady2009@gmail.com>
2023-03-22 22:45:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const  newYears= '11 Sep 2023';
+const days =document.getElementById('days');
+const hours =document.getElementById('hours');
+const mins=document.getElementById('mins');
+const sec=document.getElementById('seconds');
