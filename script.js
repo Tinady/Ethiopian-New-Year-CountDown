93c4d9567a1bb57e40957383e89dd144cb1dc489
Tina Dessu <tinady2009@gmail.com>
2023-03-22 22:47:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ const days =document.getElementById('days');
 const hours =document.getElementById('hours');
 const mins=document.getElementById('mins');
 const sec=document.getElementById('seconds');
+
+function countdown()
+{
+    const newyearsDate =new Date(newYears);
+    const currentDate = new Date
+    const Tseconds=(newyearsDate-currentDate)/1000
+
+
+   
+    const Days= Math.floor(Tseconds/3600/24);
+    const Hours=  Math.floor(Tseconds/3600)%24;
+    const Minutes=Math.floor(Tseconds/60)%60;
+    const Seconds=Math.floor(Tseconds)%60;
+
+
+    days.innerHTML=Days;
+   hours.innerHTML=Format(Hours);
+    mins.innerHTML=Format(Minutes);
+    sec.innerHTML=Format(Seconds);
+   
+
+
+}
